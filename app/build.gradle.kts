f046fc740f462582018d2c814087e8b74128aa2a
FAZLIDDIN <murodjonovfazliddin1999@gmail.com>
2023-09-03 18:45:14 +0500

    dictionary

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.brightfuture.dictionary"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.brightfuture.dictionary"
@@ -30,15 +30,15 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     buildFeatures {
-        viewBinding = true
+        viewBinding  = true
     }
 }
 
@@ -48,13 +48,13 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.6.0")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.1")
     implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
-    implementation ("com.github.kirich1409:viewbindingpropertydelegate:1.5.9")
+    implementation ("com.github.kirich1409:viewbindingpropertydelegate-full:1.5.9")
     implementation ("com.github.kirich1409:viewbindingpropertydelegate-noreflection:1.5.9")
     implementation ("com.squareup.retrofit2:converter-scalars:2.9.0")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
@@ -73,7 +73,7 @@ dependencies {
     implementation("androidx.room:room-guava:2.5.2")
     implementation ("androidx.room:room-paging:2.5.2")
     implementation ("io.reactivex.rxjava3:rxandroid:3.0.2")
-    implementation ("io.reactivex.rxjava3:rxjava:3.1.6")
+    implementation ("io.reactivex.rxjava3:rxjava:3.1.7")
     implementation ("io.reactivex:rxandroid:1.2.1")
     implementation ("io.reactivex:rxjava:1.3.8")
     implementation("androidx.room:room-rxjava2:2.5.2")
--- app/src/main/java/com/brightfuture/dictionary/MainActivity.kt
+++ app/src/main/java/com/brightfuture/dictionary/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.brightfuture.dictionary
 
+import android.content.res.ColorStateList
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.brightfuture.dictionary.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+    private val binding: ActivityMainBinding by viewBinding()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding.appBarMain.contentMain.btnNav.itemIconTintList = null
+        val rippleColor = ColorDrawable(ContextCompat.getColor(this, R.color.red_four))
+        val colorStateList = ColorStateList.valueOf(ContextCompat.getColor(this,R.color.red_four))
+
+        //binding.appBarMain.contentMain.btnNav.itemRippleColor =colorStateList
+
     }
 }
